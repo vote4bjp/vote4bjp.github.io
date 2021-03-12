a3f91d4dfcd96a6efda4f67bd132b04fe7498cdc
NanduKkd <nandukkd7164@gmail.com>
2021-03-12 17:50:28 +0530

    Data updated

---
+++ static/js/12.d675e853.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpvote4bjp=this.webpackJsonpvote4bjp||[]).push([[12],{36:function(p,t,a){"use strict";a.r(t),t.default='\u0d15\u0d47\u0d30\u0d33\u0d24\u0d4d\u0d24\u0d3f\u0d7d \u0d17\u0d41\u0d23\u0d2d\u0d4b\u0d15\u0d4d\u0d24\u0d3e\u0d15\u0d4d\u0d15\u0d33\u0d3e\u0d2f \u0d15\u0d41\u0d1f\u0d41\u0d02\u0d2c\u0d19\u0d4d\u0d19\u0d7e :- <b>21,50,000</b><br><br>- PMJAY \u0d35\u0d46\u0d2c\u0d4d\u0d38\u0d48\u0d31\u0d4d\u0d31\u0d4d<br><a href="https://pmjay.gov.in/kerala-profile">https://pmjay.gov.in/kerala-profile</a>'}}]);
+//# sourceMappingURL=12.d675e853.chunk.js.map
---
+++ static/js/8.af456505.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpvote4bjp=this.webpackJsonpvote4bjp||[]).push([[8],{32:function(t,p,s){"use strict";s.r(p),p.default=" "}}]);
+//# sourceMappingURL=8.af456505.chunk.js.map
